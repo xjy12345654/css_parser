31748006d2cc6c7b3a1a30fb0bd50ec7391e1bce
xujyang <xjy12345654@163.com>
2025-05-09 17:18:06 +0800

    Add custom directory

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 **功能： 将 px 转换为 rem 或 vw/vh。**<br>
-**css解析器**为 lightningcss(rust_lib)<br>
-**启动**  cargo run<br>
+**Rust_version**: 1.85<br>
+**GUI**: Slint 1.10.0<br>
+**cssparser**: lightningcss(rust_lib)<br>
+
+**编译**需安装Rust开发环境<br>
+**启动**cargo run<br>
 **生成优化后的可执行程序**  cargo build --release<br>
 <br>
 注意事项:
 1. 媒体查询 @media screen and (max-width: 300px)的范围设置不进行单位转换，1px 的绝对值不进行单位转换，当然可以根据情况修改。
-2. 需要转换的 css 文件放入 css_files 文件夹中 。
-3. 运行程序与 css_files 目录在同级目录下。
-4. 需要转换的 css 文件的文件名避免含有\_conv 字符串，否则将被忽略无法转换。
-5. vw 模式情况下 正常写法将全部转换成 vw, 如需要 vh: 写 vh(数值)来进行 vh 的转换
+2. 需要转换的 css 文件的文件名避免含有\_conv_rem 或者\_conv_vw 字符串，否则将被忽略无法转换。
+3. vw 模式情况下 正常写法将全部转换成 vw, 如需要 vh: 写 vh(数值)来进行 vh 的转换
    例： .foo {
    width: 32px;
    height: vh(60);
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::error::Error;
-slint::include_modules!();
-use ::css_parser::{PaOptions, read_file};
+mod app;
+use crate::app::AppController;
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let ui = AppWindow::new()?;
+ 
+    // let ui = AppWindow::new()?;
     // let window = ui.window();
     // println!("{:?}", window.position());
     // println!("{:?}", window.scale_factor());
@@ -39,51 +41,88 @@ fn main() -> Result<(), Box<dyn Error>> {
     //         }
     //     });
 
-    ui.on_btnclicked({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            let checktype = ui.get_selected_index();
-            let pa_option = if checktype == "0" {
-                // println!("rem");
-                let str_num = ui.get_be_fontsize();
-                let num: f32 = str_num.parse().unwrap();
-                PaOptions {
-                    font_num: Some(num),
-                    checktype: Some(checktype.as_str()),
-                    ..Default::default()
-                }
-                // let _ = read_file(pa_option);
-            } else {
-                let be_width = ui.get_be_width();
-                let be_height = ui.get_be_height();
-                let be_width: f32 = be_width.parse().unwrap();
-                let be_height: f32 = be_height.parse().unwrap();
-                // println!("{}",be_height);
-                // println!("vw");
-                PaOptions {
-                    be_width: Some(be_width),
-                    be_height: Some(be_height),
-                    checktype: Some(checktype.as_str()),
-                    ..Default::default()
-                }
+    // ui.on_btnclicked({
+    //     let ui_handle = ui.as_weak();
+    //     move || {
 
-                // let _ = read_file(pa_option);
-            };
-            let _res: Result<(), Box<dyn Error>> = read_file(pa_option);
+    //         let ui = ui_handle.unwrap();
+    //         let checktype = ui.get_selected_index();
+    //         let file_path: String = ui.invoke_getfilepath().into();
+    //         if file_path.is_empty() {
+    //             // println!("文件路径为空_{}", file_path);
+    //             ui.invoke_msgerr_pop(0);
+    //             return;
+    //         }
+    //         // println!("path_{}", file_path);
+    //         let pa_option = if checktype == "0" {
+    //             let str_num = ui.get_be_fontsize();
+    //             let num: f32 = str_num.parse().unwrap();
+    //             println!("{}", num);
+    //             PaOptions {
+    //                 font_num: Some(num),
+    //                 checktype: Some(checktype.as_str()),
+    //                 file_path: Some(&file_path),
+    //                 ..Default::default()
+    //             }
+    //         } else {
+    //             let be_width = ui.get_be_width();
+    //             let be_height = ui.get_be_height();
+    //             let be_width: f32 = be_width.parse().unwrap();
+    //             let be_height: f32 = be_height.parse().unwrap();
 
-            match _res {
-                Ok(()) => {
-                    ui.invoke_msgsuc_pop();
-                }
-                Err(_e) => {
-                    // ui.set_msgerr_pop_opacity_flag(true);
-                    ui.invoke_msgerr_pop();
-                }
-            };
-        }
-    });
+    //             PaOptions {
+    //                 be_width: Some(be_width),
+    //                 be_height: Some(be_height),
+    //                 checktype: Some(checktype.as_str()),
+    //                 file_path: Some(&file_path),
+    //                 ..Default::default()
+    //             }
 
-    ui.run()?;
-    Ok(())
+    //             // let _ = read_file(pa_option);
+    //         };
+    //         let _res: Result<(), Box<dyn Error>> = read_file(pa_option);
+
+    //         match _res {
+    //             Ok(()) => {
+    //                 ui.invoke_msgsuc_pop();
+    //             }
+    //             Err(e) => {
+    //                 // println!("{}", e);
+    //                 match e.downcast_ref::<NoCssFilesFound>() {
+    //                     Some(_e) => {
+    //                         ui.invoke_msgerr_pop(2);
+    //                         return;
+    //                     }
+    //                     _ => {}
+    //                 }
+    //                 ui.invoke_msgerr_pop(1);
+    //             }
+    //         };
+    //     }
+    // });
+
+    // ui.on_select_file({
+    //     let ui_handle = ui.as_weak();
+    //     move || {
+    //         let ui = ui_handle.unwrap();
+    //         let selected_path = FileDialog::new().set_title("select FilePath").pick_folder();
+    //         match selected_path {
+    //             Some(path) => {
+    //                 // println!("选择的文件夹路径: {}", path.display());
+    //                 let flie_path = path.display().to_string();
+    //                 ui.invoke_flie_inputte(flie_path.into());
+    //             }
+    //             None => {
+    //                 // println!("取消了选择");
+    //             }
+    //         }
+    //     }
+    // });
+
+    // ui.run()?;
+    // Ok(())
+    let app = AppController::new()?;
+    app.setup_event_handlers();
+    app.run()
+   
 }
